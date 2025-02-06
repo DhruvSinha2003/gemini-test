7a2df90003b815152cd8ffb7d99714425c438643
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-06 17:52:18 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import axios from "axios";
+import "dotenv/config";
 function App() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  async function getResult(input) {
+    try {
+      const response = await axios({
+        method: "post",
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+        params: {
+          key: API_KEY,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          contents: [
+            {
+              parts: [
+                {
+                  text: `${input}`,
+                },
+              ],
+            },
+          ],
+        },
+      });
+
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
   return (
     <>
-      <h1 className="bg-red">Hello</h1>
+      <h1 className=" bg-red-500">Hello</h1>
+      <form action="submit">
+        <input type="text" />
+      </form>
     </>
   );
 }
